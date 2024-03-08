cadd4dd1f9383f9931a53506cb8980b99b8935aa
Jewel-cse <md.jewel.cse@gmail.com>
2024-03-09 04:32:05 +0600

    print all the element : functional programming

---
+++ src/programming/FP1_printList.java
@@ -0,0 +1,20 @@
+package programming;
+
+import java.util.List;
+
+public class FP1_printList {
+    public static void main(String[] args) {
+        printListElement(List.of(1,2,3,4,5));
+    }
+
+    //Functional programming
+    //what to do
+    //function reference  apply
+    private static void printListElement(List<Integer> numbers) {
+        numbers.stream().forEach(FP1_printList::print);
+    }
+
+    private static void print(Integer integer) {
+        System.out.println(integer);
+    }
+}
